280e3fa70a09d6e287215e2f77a14898bb7b4772
juliaam <juliademoraess@gmail.com>
2024-08-05 19:12:02 -0300

    fix: backend prisma ready

--- src/features/loan/repositorys/loan.repository.ts
+++ src/features/loan/repositorys/loan.repository.ts
@@ -1,18 +1,8 @@
 import { prisma } from "../../../services/prisma";
 import { ILoan } from "../controllers/loan.controller";
-import { Loan } from "../../loan-sequelize/models/loan-model";
-// export const createLoan = async (data: ILoan) => {
-//   return await prisma.loan.create({
-//     data,
-//   });
-// };
 
 export const createLoan = async (data: ILoan) => {
-  try {
-    const newLoan = await Loan.create(data);
-    return newLoan;
-  } catch (error) {
-    console.error("Error creating loan:", error);
-    throw error;
-  }
+  return await prisma.loan.create({
+    data,
+  });
 };
