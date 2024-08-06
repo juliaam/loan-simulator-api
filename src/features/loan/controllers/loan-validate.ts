4aaa7edcf597e032fa0e5812bd37fe88fac652e5
juliaam <juliademoraess@gmail.com>
2024-08-06 02:23:55 -0300

    feat: validate

--- src/features/loan/controllers/loan-validate.ts
+++ src/features/loan/controllers/loan-validate.ts
@@ -9,7 +9,7 @@ export const loanSchema = z.object({
   }),
   birth: z
     .string()
-    .regex(/^\d{2}\/\d{2}\/\d{4}$/, {
+    .regex(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d\d$/, {
       message: "Data de nascimento deve estar no formato DD/MM/YYYY",
     }),
   total_value: z.number().nonnegative({
